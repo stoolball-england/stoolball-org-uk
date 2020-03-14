2984b4338e297f2b9dd6bc65f3a41299877bd273
Rick Mason <rick@stoolball.org.uk>
2020-03-14 12:39:58 +0000

    Update the pull from Umbraco Cloud workflow to require manual review.

--- Scripts/Pull-UmbracoCloud.ps1
+++ Scripts/Pull-UmbracoCloud.ps1
@@ -9,13 +9,17 @@ git pull
 robocopy . ..\Stoolball.Web *.uda *.cshtml /S /XD .git
 Pop-Location
 
-# Commit the new changes to Stoolball.Web on master. master is now up-to-date with Umbraco Cloud.
-git add .
-git commit -m "Merging changes from remote into master using Pull-UmbracoCloud.ps1"
-
 # Trigger an update from remote to local by Umbraco deploy
 if (!(Test-Path "./Stoolball.Web/data/deploy" -PathType Leaf)) {
     New-Item -Path "./Stoolball.Web/data" -Name "deploy" -ItemType "file"
 }
+
+Write-Host "THIS COULD LOSE YOUR WORK.
+
+Review the git status below and commit changes to master if appropriate.
+master will then be up-to-date with Umbraco Cloud." -ForegroundColor Red
+git add .
+git status
+
 Pop-Location
 
