0f65f5d017487dbf9c5ad981c5fc045cc47ef491
Rick Mason <rick@stoolball.org.uk>
2020-03-21 10:36:42 +0000

    Revise planned tables to accomodate long-planned future improvements

--- Stoolball.Umbraco.Migrations/Constants.cs
+++ Stoolball.Umbraco.Migrations/Constants.cs
@@ -12,28 +12,30 @@
         public static class Tables
         {
             public const string Audit = _tablePrefix + "Audit";
-            public const string Queue = _tablePrefix + "Queue";
 
             public const string NotificationSubscription = _tablePrefix + "NotificationSubscription";
-            public const string NotificationSent = _tablePrefix + "NotificationSent";
+            public const string NotificationTracker = _tablePrefix + "NotificationTracker";
 
             public const string Batting = _tablePrefix + "Batting";
             public const string Bowling = _tablePrefix + "Bowling";
             public const string Club = _tablePrefix + "Club";
+            public const string ClubName = _tablePrefix + "ClubName";
             public const string Competition = _tablePrefix + "Competition";
-            public const string Ground = _tablePrefix + "Ground";
             public const string Match = _tablePrefix + "Match";
             public const string MatchComment = _tablePrefix + "MatchComment";
+            public const string MatchLocation = _tablePrefix + "MatchLocation";
             public const string MatchTeam = _tablePrefix + "MatchTeam";
             public const string PlayerIdentity = _tablePrefix + "PlayerIdentity";
             public const string School = _tablePrefix + "School";
             public const string Season = _tablePrefix + "Season";
             public const string SeasonMatch = _tablePrefix + "SeasonMatch";
             public const string SeasonMatchType = _tablePrefix + "SeasonMatchType";
-            public const string SeasonPoint = _tablePrefix + "SeasonPoint";
+            public const string SeasonPointsAdjustment = _tablePrefix + "SeasonPointsAdjustment";
             public const string SeasonPointsRule = _tablePrefix + "SeasonPointsRule";
             public const string SeasonTeam = _tablePrefix + "SeasonTeam";
             public const string Team = _tablePrefix + "Team";
+            public const string TeamName = _tablePrefix + "TeamName";
+            public const string TeamMatchLocation = _tablePrefix + "TeamMatchLocation";
 
             public const string PlayerMatchStatistics = _tablePrefix + _statisticsPrefix + "PlayerMatch";
         }
