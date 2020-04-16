34d9d29566d421e3a97d524f625b79cb1ce5dce9
Rick Mason <rick@stoolball.org.uk>
2020-04-16 21:17:21 +0100

    Push images up to Umbraco Cloud, and ensure a pull is done first to avoid git sync errors.

--- Scripts/Push-UmbracoCloud.ps1
+++ Scripts/Push-UmbracoCloud.ps1
@@ -11,12 +11,13 @@ Write-Host "THIS COULD LOSE YOUR WORK. ARE YOU SURE?
 Read-Host -Prompt "Press any key to continue or CTRL+C to quit"
 
 # Copy changes from Stoolball.Web to the .UmbracoCloud deployment repository
-robocopy .\Stoolball.Web .\.UmbracoCloud /IF *.dll *.cshtml *.uda *.xdt.config *.css *.html *.js package.manifest en-*.xml /XF Umbraco.*.dll member-group__*.uda /S /XD .git $projectRoot\Stoolball.Web\obj $projectRoot\Stoolball.Web\umbraco $projectRoot\Stoolball.Web\App_Data $projectRoot\Stoolball.Web\App_Plugins\Deploy $projectRoot\Stoolball.Web\App_Plugins\UmbracoForms $projectRoot\Stoolball.Web\Content $projectRoot\Stoolball.Web\Media
+robocopy .\Stoolball.Web .\.UmbracoCloud /IF *.dll *.cshtml *.uda *.xdt.config *.css *.html *.js package.manifest en-*.xml *.png *.gif *.jpg /XF Umbraco.*.dll member-group__*.uda /S /XD .git $projectRoot\Stoolball.Web\obj $projectRoot\Stoolball.Web\umbraco $projectRoot\Stoolball.Web\App_Data $projectRoot\Stoolball.Web\App_Plugins\Deploy $projectRoot\Stoolball.Web\App_Plugins\UmbracoForms $projectRoot\Stoolball.Web\Content $projectRoot\Stoolball.Web\Media
 
 # Commit and push those changes
 Push-Location .\.UmbracoCloud
 git add .
 git commit -am "Changes from local site"
+git pull origin master
 git push origin master
 Pop-Location
 
