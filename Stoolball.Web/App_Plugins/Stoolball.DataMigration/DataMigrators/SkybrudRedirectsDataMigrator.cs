1565b839373e09ef71833726a219c062dadfb729
Rick Mason <rick@stoolball.org.uk>
2020-06-03 20:01:58 +0100

    Preserve redirects for old insurance URLs. Fixes #16.

--- Stoolball.Web/App_Plugins/Stoolball.DataMigration/DataMigrators/SkybrudRedirectsDataMigrator.cs
+++ Stoolball.Web/App_Plugins/Stoolball.DataMigration/DataMigrators/SkybrudRedirectsDataMigrator.cs
@@ -6,60 +6,64 @@ using Umbraco.Web;
 
 namespace Stoolball.Web.AppPlugins.Stoolball.DataMigration.DataMigrators
 {
-	/// <summary>
-	/// Create redirects from the old website that are not related to a particular stoolball data entity
-	/// </summary>
-	public class SkybrudRedirectsDataMigrator : IRedirectsDataMigrator
-	{
-		private readonly IRedirectsRepository _redirectsRepository;
-		private readonly ILogger _logger;
-		public SkybrudRedirectsDataMigrator(IRedirectsRepository redirectsRepository, ILogger logger)
-		{
-			_redirectsRepository = redirectsRepository ?? throw new ArgumentNullException(nameof(redirectsRepository));
-			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
-		}
+    /// <summary>
+    /// Create redirects from the old website that are not related to a particular stoolball data entity
+    /// </summary>
+    public class SkybrudRedirectsDataMigrator : IRedirectsDataMigrator
+    {
+        private readonly IRedirectsRepository _redirectsRepository;
+        private readonly ILogger _logger;
+        public SkybrudRedirectsDataMigrator(IRedirectsRepository redirectsRepository, ILogger logger)
+        {
+            _redirectsRepository = redirectsRepository ?? throw new ArgumentNullException(nameof(redirectsRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
 
-		public async Task EnsureRedirects(IPublishedContentQuery publishedContentQuery)
-		{
-			if (publishedContentQuery is null)
-			{
-				throw new ArgumentNullException(nameof(publishedContentQuery));
-			}
+        public async Task EnsureRedirects(IPublishedContentQuery publishedContentQuery)
+        {
+            if (publishedContentQuery is null)
+            {
+                throw new ArgumentNullException(nameof(publishedContentQuery));
+            }
 
-			try
-			{
-				await _redirectsRepository.DeleteRedirectsByDestinationPrefix("/you").ConfigureAwait(false);
+            try
+            {
+                await _redirectsRepository.DeleteRedirectsByDestinationPrefix("/you").ConfigureAwait(false);
 
-				var accountPage = publishedContentQuery.ContentSingleAtXPath("//myAccount");
-				if (accountPage != null)
-				{
-					await _redirectsRepository.InsertRedirect("/you/settings.php", accountPage.Id, accountPage.Key, new Uri(accountPage.Url, UriKind.Relative)).ConfigureAwait(false);
-					await _redirectsRepository.InsertRedirect("/you/essential.php", accountPage.Id, accountPage.Key, new Uri(accountPage.Url, UriKind.Relative)).ConfigureAwait(false);
-				}
+                var accountPage = publishedContentQuery.ContentSingleAtXPath("//myAccount");
+                if (accountPage != null)
+                {
+                    await _redirectsRepository.InsertRedirect("/you/settings.php", accountPage.Id, accountPage.Key, new Uri(accountPage.Url, UriKind.Relative)).ConfigureAwait(false);
+                    await _redirectsRepository.InsertRedirect("/you/essential.php", accountPage.Id, accountPage.Key, new Uri(accountPage.Url, UriKind.Relative)).ConfigureAwait(false);
+                }
 
-				var loginPage = publishedContentQuery.ContentSingleAtXPath("//loginMember");
-				if (loginPage != null)
-				{
-					await _redirectsRepository.InsertRedirect("/you", loginPage.Id, loginPage.Key, new Uri(loginPage.Url, UriKind.Relative)).ConfigureAwait(false);
-				}
+                var loginPage = publishedContentQuery.ContentSingleAtXPath("//loginMember");
+                if (loginPage != null)
+                {
+                    await _redirectsRepository.InsertRedirect("/you", loginPage.Id, loginPage.Key, new Uri(loginPage.Url, UriKind.Relative)).ConfigureAwait(false);
+                }
 
-				var passwordResetPage = publishedContentQuery.ContentSingleAtXPath("//resetPassword");
-				if (passwordResetPage != null)
-				{
-					await _redirectsRepository.InsertRedirect("/you/request-password-reset", passwordResetPage.Id, passwordResetPage.Key, new Uri(passwordResetPage.Url, UriKind.Relative)).ConfigureAwait(false);
-				}
+                var passwordResetPage = publishedContentQuery.ContentSingleAtXPath("//resetPassword");
+                if (passwordResetPage != null)
+                {
+                    await _redirectsRepository.InsertRedirect("/you/request-password-reset", passwordResetPage.Id, passwordResetPage.Key, new Uri(passwordResetPage.Url, UriKind.Relative)).ConfigureAwait(false);
+                }
 
-				var createMemberPage = publishedContentQuery.ContentSingleAtXPath("//createMember");
-				if (createMemberPage != null)
-				{
-					await _redirectsRepository.InsertRedirect("/you/signup.php", createMemberPage.Id, createMemberPage.Key, new Uri(createMemberPage.Url, UriKind.Relative)).ConfigureAwait(false);
-				}
-			}
-			catch (Exception e)
-			{
-				_logger.Error<SkybrudRedirectsDataMigrator>(e);
-				throw;
-			}
-		}
-	}
+                var createMemberPage = publishedContentQuery.ContentSingleAtXPath("//createMember");
+                if (createMemberPage != null)
+                {
+                    await _redirectsRepository.InsertRedirect("/you/signup.php", createMemberPage.Id, createMemberPage.Key, new Uri(createMemberPage.Url, UriKind.Relative)).ConfigureAwait(false);
+                }
+
+                await _redirectsRepository.DeleteRedirectsByDestinationPrefix("/play/manage/insurance").ConfigureAwait(false);
+                await _redirectsRepository.InsertRedirect("/insurance", "/play/manage/insurance", null).ConfigureAwait(false);
+                await _redirectsRepository.InsertRedirect("/manage/insurance", "/play/manage/insurance", null).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                _logger.Error<SkybrudRedirectsDataMigrator>(e);
+                throw;
+            }
+        }
+    }
 }
