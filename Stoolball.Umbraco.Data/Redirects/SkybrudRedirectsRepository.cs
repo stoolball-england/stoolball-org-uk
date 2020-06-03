5c3d0cd450ccaecc4d0bedfe7a9101f80e3bbbb3
Rick Mason <rick@stoolball.org.uk>
2020-06-03 20:32:12 +0100

    Redirects to account pages from old URLs were not being inserted. Fixes #139.

--- Stoolball.Umbraco.Data/Redirects/SkybrudRedirectsRepository.cs
+++ Stoolball.Umbraco.Data/Redirects/SkybrudRedirectsRepository.cs
@@ -5,108 +5,113 @@ using Umbraco.Core.Scoping;
 
 namespace Stoolball.Umbraco.Data.Redirects
 {
-	public class SkybrudRedirectsRepository : IRedirectsRepository
-	{
-		private readonly IScopeProvider _scopeProvider;
-		private readonly ILogger _logger;
+    public class SkybrudRedirectsRepository : IRedirectsRepository
+    {
+        private readonly IScopeProvider _scopeProvider;
+        private readonly ILogger _logger;
 
-		public SkybrudRedirectsRepository(IScopeProvider scopeProvider, ILogger logger)
-		{
-			_scopeProvider = scopeProvider ?? throw new ArgumentNullException(nameof(scopeProvider));
-			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
-		}
+        public SkybrudRedirectsRepository(IScopeProvider scopeProvider, ILogger logger)
+        {
+            _scopeProvider = scopeProvider ?? throw new ArgumentNullException(nameof(scopeProvider));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
 
-		public async Task DeleteRedirectsByDestinationPrefix(string destinationPrefix)
-		{
-			try
-			{
-				using (var scope = _scopeProvider.CreateScope())
-				{
-					var database = scope.Database;
+        public async Task DeleteRedirectsByDestinationPrefix(string destinationPrefix)
+        {
+            try
+            {
+                using (var scope = _scopeProvider.CreateScope())
+                {
+                    var database = scope.Database;
 
-					await database.ExecuteAsync($@"DELETE FROM SkybrudRedirects WHERE DestinationUrl LIKE '{destinationPrefix}%'").ConfigureAwait(false);
+                    await database.ExecuteAsync($@"DELETE FROM SkybrudRedirects WHERE DestinationUrl LIKE '{destinationPrefix}%'").ConfigureAwait(false);
 
-					scope.Complete();
-				}
-			}
-			catch (Exception e)
-			{
-				_logger.Error<SkybrudRedirectsRepository>(e);
-				throw;
-			}
-		}
+                    scope.Complete();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Error<SkybrudRedirectsRepository>(e);
+                throw;
+            }
+        }
 
-		public async Task InsertRedirect(string originalRoute, string revisedRoute, string routeSuffix)
-		{
-			try
-			{
-				using (var scope = _scopeProvider.CreateScope())
-				{
-					var database = scope.Database;
+        public async Task InsertRedirect(string originalRoute, string revisedRoute, string routeSuffix)
+        {
+            try
+            {
+                using (var scope = _scopeProvider.CreateScope())
+                {
+                    var database = scope.Database;
 
-					await database.ExecuteAsync($@"INSERT INTO SkybrudRedirects 
+                    await database.ExecuteAsync($@"INSERT INTO SkybrudRedirects 
 							([Key], [RootId], [RootKey], [Url], [QueryString], [DestinationType], [DestinationId], [DestinationKey], 
 							 [DestinationUrl], [Created], [Updated], [IsPermanent], [IsRegex], [ForwardQueryString])
 							 VALUES (@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13)",
-								 Guid.NewGuid().ToString(),
-								 0,
-								 "00000000-0000-0000-0000-000000000000",
-								 "/" + originalRoute?.TrimStart('/') + routeSuffix,
-								 string.Empty,
-								 "url",
-								 0,
-								 "00000000-0000-0000-0000-000000000000",
-								 "/" + revisedRoute?.TrimStart('/') + routeSuffix,
-								 DateTime.UtcNow,
-								 DateTime.UtcNow,
-								 true,
-								 false,
-								 false
-								 ).ConfigureAwait(false);
-					scope.Complete();
-				}
-			}
-			catch (Exception e)
-			{
-				_logger.Error<SkybrudRedirectsRepository>(e);
-				throw;
-			}
-		}
+                                 Guid.NewGuid().ToString(),
+                                 0,
+                                 "00000000-0000-0000-0000-000000000000",
+                                 "/" + originalRoute?.TrimStart('/') + routeSuffix,
+                                 string.Empty,
+                                 "url",
+                                 0,
+                                 "00000000-0000-0000-0000-000000000000",
+                                 "/" + revisedRoute?.TrimStart('/') + routeSuffix,
+                                 DateTime.UtcNow,
+                                 DateTime.UtcNow,
+                                 true,
+                                 false,
+                                 false
+                                 ).ConfigureAwait(false);
+                    scope.Complete();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Error<SkybrudRedirectsRepository>(e);
+                throw;
+            }
+        }
 
-		public async Task InsertRedirect(string originalRoute, int umbracoContentNodeId, Guid umbracoContentNodeKey, Uri umbracoContentNodeUrl)
-		{
-			try
-			{
-				using (var scope = _scopeProvider.CreateScope())
-				{
-					var database = scope.Database;
+        public async Task InsertRedirect(string originalRoute, int umbracoContentNodeId, Guid umbracoContentNodeKey, Uri umbracoContentNodeUrl)
+        {
+            if (umbracoContentNodeUrl is null)
+            {
+                throw new ArgumentNullException(nameof(umbracoContentNodeUrl));
+            }
 
-					await database.ExecuteAsync($@"INSERT INTO SkybrudRedirects 
+            try
+            {
+                using (var scope = _scopeProvider.CreateScope())
+                {
+                    var database = scope.Database;
+
+                    await database.ExecuteAsync($@"INSERT INTO SkybrudRedirects 
 							([Key], [RootId], [RootKey], [Url], [QueryString], [DestinationType], [DestinationId], [DestinationKey], 
 							 [DestinationUrl], [Created], [Updated], [IsPermanent], [IsRegex], [ForwardQueryString])
 							 VALUES (@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13)",
-									 Guid.NewGuid().ToString(),
-									 0,
-									 "00000000-0000-0000-0000-000000000000",
-									 "/" + originalRoute?.TrimStart('/'),
-									 string.Empty,
-									 "url",
-									 umbracoContentNodeId,
-									 umbracoContentNodeKey,
-									 umbracoContentNodeUrl,
-									 DateTime.UtcNow,
-									 DateTime.UtcNow,
-									 true,
-									 false,
-									 false).ConfigureAwait(false);
-					scope.Complete();
-				}
-			}
-			catch (Exception e)
-			{
-				_logger.Error<SkybrudRedirectsRepository>(e);
-				throw;
-			}
-		}
-	}
+                                     Guid.NewGuid().ToString(),
+                                     0,
+                                     "00000000-0000-0000-0000-000000000000",
+                                     "/" + originalRoute?.TrimStart('/'),
+                                     string.Empty,
+                                     "url",
+                                     umbracoContentNodeId,
+                                     umbracoContentNodeKey,
+                                     umbracoContentNodeUrl.ToString(),
+                                     DateTime.UtcNow,
+                                     DateTime.UtcNow,
+                                     true,
+                                     false,
+                                     false).ConfigureAwait(false);
+                    scope.Complete();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Error<SkybrudRedirectsRepository>(e);
+                throw;
+            }
+        }
+    }
 }
