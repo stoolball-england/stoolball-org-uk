24812f7cc2021fd4119f13b746c584e82094bd59
Rick Mason <rick@stoolball.org.uk>
2020-06-06 16:18:24 +0100

    Use the word 'junior' consistently to refer to all junior player types.

--- Stoolball/Teams/PlayerType.cs
+++ Stoolball/Teams/PlayerType.cs
@@ -1,35 +1,35 @@
 ﻿namespace Stoolball.Teams
 {
-	public enum PlayerType
-	{
-		/// <summary>
-		/// A match involving men, women, girls and boys
-		/// </summary>
-		Mixed,
+    public enum PlayerType
+    {
+        /// <summary>
+        /// A match involving men, women, girls and boys
+        /// </summary>
+        Mixed,
 
-		/// <summary>
-		/// A match involving only women and girls
-		/// </summary>
-		Ladies,
+        /// <summary>
+        /// A match involving only women and girls
+        /// </summary>
+        Ladies,
 
-		/// <summary>
-		/// A match involving only men and boys
-		/// </summary>
-		Men,
+        /// <summary>
+        /// A match involving only men and boys
+        /// </summary>
+        Men,
 
-		/// <summary>
-		/// A match involving girls and boys
-		/// </summary>
-		JuniorMixed,
+        /// <summary>
+        /// A match involving girls and boys
+        /// </summary>
+        JuniorMixed,
 
-		/// <summary>
-		/// A match involving only girls
-		/// </summary>
-		Girls,
+        /// <summary>
+        /// A match involving only girls
+        /// </summary>
+        JuniorGirls,
 
-		/// <summary>
-		/// A match involving only boys
-		/// </summary>
-		Boys
-	}
+        /// <summary>
+        /// A match involving only boys
+        /// </summary>
+        JuniorBoys
+    }
 }
