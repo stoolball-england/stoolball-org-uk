9f2e18e72a3fb66c3c516226fe6e28ecac2f3ad2
Rick Mason <caterwomtious@gmail.com>
2020-09-05 10:15:18 +0100

    Optimise images and metadata for basic offline page.

--- Stoolball.Web/service-worker.js
+++ Stoolball.Web/service-worker.js
@@ -5,9 +5,10 @@ const cacheIDs = [coreCache];
 // Cache a branded offline page
 self.addEventListener('install', function (event) {
 	event.waitUntil(caches.open(coreCache).then(function (cache) {
-		cache.add(new Request('/httpstatus/offline.html'));
+		cache.add(new Request('/status/offline.html'));
 		cache.add(new Request('/css/base.min.css'));
 		cache.add(new Request('/favicon.ico'));
+		cache.add(new Request('/images/logos/stoolball-england-icon.svg'));
 		cache.add(new Request('/images/logos/stoolball-england.svg'));
 		return;
 	}));
@@ -40,7 +41,7 @@ self.addEventListener('fetch', function (event) {
 			fetch(request).then(function (response) {
 				return response;
 			}).catch(function (error) {
-				return caches.match('/httpstatus/offline.html');
+				return caches.match('/status/offline.html');
 			})
 		)
 	}
