c377bf287f6d15ec9cf75ebbc4e2d58395505672
Rick Mason <caterwomtious@gmail.com>
2020-09-05 11:31:24 +0100

    Reenable default values for bowling scorecard #109

--- Stoolball.Web/Matches/edit-bowling-scorecard.js
+++ Stoolball.Web/Matches/edit-bowling-scorecard.js
@@ -1,129 +1,133 @@
 (function () {
   window.addEventListener("DOMContentLoaded", function () {
-    const editor = document.getElementsByClassName(
-      "bowling-scorecard-editor"
-    )[0];
+    const editor = document.querySelector(".bowling-scorecard-editor");
 
-    function disableFollowingRows(tr) {
-      while (tr.nextElementSibling) {
-        var thisPlayer = tr.querySelector(".scorecard__player-name");
-        var nextPlayer = tr.nextElementSibling.querySelector(
+    function disableFollowingRows(row) {
+      while (row.nextElementSibling) {
+        let thisPlayer = row.querySelector(".scorecard__player-name");
+        let nextPlayer = row.nextElementSibling.querySelector(
           ".scorecard__player-name"
         );
         if (!thisPlayer.value && !nextPlayer.value) {
           nextPlayer.setAttribute("disabled", "disabled");
         }
-        tr = tr.nextElementSibling;
+        row = row.nextElementSibling;
       }
     }
 
+    function focusEvent(e) {
+      if (e.target.classList.contains("scorecard__player-name")) {
+        suggestDefaultBowler(e);
+      }
+      if (e.target.classList.contains("numeric")) {
+        suggestBowlingDefaults(e);
+      }
+    }
     // Suggest defaults for bowling stats
-    //   function suggestBowlingDefaults(e) {
-    //     var thisField = $(e.target);
+    function suggestBowlingDefaults(e) {
+      // Is this field empty?
+      if (e.target.value.trim().length > 0) {
+        e.target.select();
+        return;
+      }
 
-    //     // Is this field empty?
-    //     if ($.trim(e.target.value).length > 0) {
-    //       thisField.select();
-    //       return;
-    //     }
+      // Is there a player?
+      const row = e.target.parentNode.parentNode;
+      const playerField = row.querySelector(".scorecard__player-name");
+      if (playerField.value.trim().length === 0) return;
 
-    //     // Is there a player?
-    //     var tableRow = e.target.parentNode.parentNode;
-    //     var playerField = tableRow.firstChild.firstChild;
-    //     if ($.trim(playerField.value).length == 0) return;
+      // If this over was bowled, previous overs must have been, so set balls bowled to 8 if missing
+      let previous = row.previousElementSibling,
+        previousPlayer,
+        previousBalls;
+      while (previous) {
+        previousPlayer = previous.querySelector(".scorecard__player-name");
+        if (previousPlayer.value.trim().length > 0) {
+          previousBalls = previous.querySelector(".scorecard__balls");
+          if (previousBalls.value.trim().length === 0) {
+            previousBalls.value = 8;
+          }
+        }
+        previous = previous.previousElementSibling;
+      }
 
-    //     // If this over was bowled, previous overs must have been, so set balls bowled to 8 if missing
-    //     var previous = tableRow.previousSibling,
-    //       previousPlayer,
-    //       previousBalls;
-    //     while (previous) {
-    //       previousPlayer = previous.firstChild.firstChild;
-    //       if ($.trim(previousPlayer.value).length > 0) {
-    //         previousBalls = previous.childNodes[1].firstChild;
-    //         if ($.trim(previousBalls.value).length == 0) {
-    //           previousBalls.value = 8;
-    //         }
-    //       }
-    //       previous = previous.previousSibling;
-    //     }
+      // // Apply defaults to current field and previous fields (previous fields useful for touch screens)
+      const ballsField = row.querySelector(".scorecard__balls");
+      const widesField = row.querySelector(".scorecard__wides");
+      const noballsField = row.querySelector(".scorecard__no-balls");
 
-    //     // Apply defaults to current field and previous fields (previous fields useful for touch screens)
-    //     var ballsField = tableRow.childNodes[1].firstChild;
-    //     var noballsField = tableRow.childNodes[2].firstChild;
-    //     var widesField = tableRow.childNodes[3].firstChild;
+      // Balls bowled defaults to 8, extras default to 0
+      if (e.target.classList.contains("scorecard__balls")) {
+        e.target.value = 8;
+      } else if (e.target.classList.contains("scorecard__no-balls")) {
+        if (ballsField.value.trim().length === 0) {
+          ballsField.value = 8;
+        }
+        e.target.value = 0;
+      } else if (e.target.classList.contains("scorecard__wides")) {
+        if (ballsField.value.trim().length === 0) {
+          ballsField.value = 8;
+        }
+        if (noballsField.value.trim().length === 0) {
+          noballsField.value = 0;
+        }
+        e.target.value = 0;
+      } else if (e.target.classList.contains("scorecard__runs")) {
+        if (ballsField.value.trim().length === 0) {
+          ballsField.value = 8;
+        }
+        if (noballsField.value.trim().length === 0) {
+          noballsField.value = 0;
+        }
+        if (widesField.value.trim().length === 0) {
+          widesField.value = 0;
+        }
+      }
+      e.target.select();
+    }
 
-    //     // Balls bowled defaults to 8, extras default to 0
-    //     if (thisField.hasClass("balls")) {
-    //       e.target.value = 8;
-    //     } else if (thisField.hasClass("no-balls")) {
-    //       if ($.trim(ballsField.value).length == 0) {
-    //         ballsField.value = 8;
-    //       }
-    //       e.target.value = 0;
-    //     } else if (thisField.hasClass("wides")) {
-    //       if ($.trim(ballsField.value).length == 0) {
-    //         ballsField.value = 8;
-    //       }
-    //       if ($.trim(noballsField.value).length == 0) {
-    //         noballsField.value = 0;
-    //       }
-    //       e.target.value = 0;
-    //     } else if (thisField.hasClass("runs")) {
-    //       if ($.trim(ballsField.value).length == 0) {
-    //         ballsField.value = 8;
-    //       }
-    //       if ($.trim(noballsField.value).length == 0) {
-    //         noballsField.value = 0;
-    //       }
-    //       if ($.trim(widesField.value).length == 0) {
-    //         widesField.value = 0;
-    //       }
-    //     }
-    //     thisField.select();
-    //   }
-
-    //   function replaceBowlingDefaults(e) {
-    //     // Best fix for bug which means Mobile Safari doesn't select text.
-    //     // This replaces default value when a number is typed, even if it's not selected.
-    //     if (e.keyCode >= 49 && e.keyCode <= 57) {
-    //       if (e.target.value == "0") e.target.value = "";
-    //     }
-    //   }
+    function replaceBowlingDefaults(e) {
+      // Best fix for bug which means Mobile Safari doesn't select text.
+      // This replaces default value when a number is typed, even if it's not selected.
+      if (!e.target.classList.contains("numeric")) return;
+      if (e.keyCode >= 49 && e.keyCode <= 57) {
+        if (e.target.value === "0") e.target.value = "";
+      }
+    }
 
     // More often than not the same bowler bowled two overs ago, so copy the name
-    //   function suggestDefaultBowler() {
-    //     // Is this field empty?
-    //     if ($.trim(this.value).length > 0) return;
+    function suggestDefaultBowler(e) {
+      // Is this field empty?
+      if (e.target.value.trim().length > 0) return;
 
-    //     // Get player name from two rows above
-    //     var tableRow = this.parentNode.parentNode;
-    //     tableRow = tableRow.previousSibling;
-    //     if (!tableRow) return;
-    //     tableRow = tableRow.previousSibling;
-    //     if (!tableRow) return;
-    //     var twoUp = tableRow.firstChild.firstChild.value;
+      // Get player name from two rows above
+      let row = e.target.parentNode.parentNode;
+      row = row.previousElementSibling;
+      if (!row) return;
+      row = row.previousElementSibling;
+      if (!row) return;
+      const twoUp = row.querySelector(".scorecard__player-name").value;
 
-    //     // If four rows above is different, the bowling is probably shared around
-    //     var fourUp;
-    //     tableRow = tableRow.previousSibling;
-    //     if (tableRow) {
-    //       tableRow = tableRow.previousSibling;
-    //       if (tableRow) {
-    //         fourUp = tableRow.firstChild.firstChild.value;
-    //       }
-    //     }
+      // If four rows above is different, the bowling is probably shared around
+      let fourUp;
+      row = row.previousElementSibling;
+      if (row) {
+        row = row.previousElementSibling;
+        if (row) {
+          fourUp = row.querySelector(".scorecard__player-name").value;
+        }
+      }
 
-    //     if (!fourUp || fourUp === twoUp) {
-    //       this.value = twoUp;
-    //       this.select();
-    //     }
-    //   }
+      if (!fourUp || fourUp === twoUp) {
+        e.target.value = twoUp;
+        e.target.select();
+      }
+    }
 
     // Add over button
-    const addOver = document.createElement("a");
-    addOver.setAttribute("href", "#");
-    addOver.setAttribute("class", "btn btn-secondary add-over");
-    //addOver.setAttribute("disabled", "disabled");
+    const addOver = document.createElement("button");
+    addOver.setAttribute("class", "btn btn-secondary");
+    addOver.setAttribute("disabled", "disabled");
     addOver.appendChild(document.createTextNode("Add an over"));
     editor.parentElement.insertBefore(addOver, editor.nextElementSibling);
 
@@ -134,9 +138,7 @@
     }
 
     function enableOver(tableRow) {
-      let playerName = tableRow.getElementsByClassName(
-        "scorecard__player-name"
-      )[0];
+      const playerName = tableRow.querySelector(".scorecard__player-name");
       const fields = tableRow.querySelectorAll("input[type=number]");
 
       if (playerName.value) {
@@ -147,12 +149,10 @@
         // If this over row used, ensure next one is ready
         if (tableRow.nextElementSibling) {
           tableRow.nextElementSibling
-            .getElementsByClassName("scorecard__player-name")[0]
+            .querySelector(".scorecard__player-name")
             .removeAttribute("disabled");
         } else {
-          document
-            .getElementsByClassName("add-over")[0]
-            .removeAttribute("disabled");
+          addOver.removeAttribute("disabled");
         }
       } else {
         for (let f = 0; f < fields.length; f++) {
@@ -163,9 +163,7 @@
         if (tableRow.nextElementSibling) {
           disableFollowingRows(tableRow);
         } else {
-          document
-            .getElementsByClassName("add-over")[0]
-            .setAttribute("disabled", "disabled");
+          addOver.setAttribute("disabled", "disabled");
         }
       }
     }
@@ -175,19 +173,14 @@
     editor.addEventListener("keyup", enableOverEvent);
     editor.addEventListener("click", enableOverEvent);
     editor.addEventListener("change", enableOverEvent);
+    editor.addEventListener("focusin", focusEvent);
+    editor.addEventListener("keydown", replaceBowlingDefaults);
 
     // Run enableOver for every row to setup the fields on page load
     const overs = editor.querySelectorAll("tbody > tr");
     for (let i = 0; i < overs.length; i++) {
       enableOver(overs[i]);
     }
-
-    //   $("input.numeric", ".bowling-scorecard-editor")
-    //     .focus(suggestBowlingDefaults)
-    //     .keydown(replaceBowlingDefaults);
-    //   $("input.player", ".bowling-scorecard-editor").focus(
-    //     suggestDefaultBowler
-    //   );
 
     // Focus first field
     editor.querySelector("input[type='text']:not(:disabled)").focus();
@@ -196,9 +189,7 @@
       e.preventDefault();
 
       // insert a new row based on a template
-      const rows = editor
-        .getElementsByTagName("tbody")[0]
-        .getElementsByTagName("tr");
+      const rows = editor.querySelectorAll("tbody > tr");
       let template = document.getElementById("over-template").innerHTML;
       rows[rows.length - 1].insertAdjacentHTML("afterend", template);
 
@@ -213,14 +204,11 @@
             .replace(/\[[0-9]+\]/, "[" + rows.length + "]")
         );
       }
-      enableOverEvent({ target: inputs[0] }); // TODO: Needs work!
+      enableOver(newRow);
 
       // focus the first field
       newRow.querySelector("input").focus();
 
-      //   $("input.numeric", newRow)
-      //     .focus(suggestBowlingDefaults)
-      //     .keydown(replaceBowlingDefaults);
       //   if (typeof stoolballAutoSuggest != "undefined")
       //     $("input.player", newRow).each(
       //       stoolballAutoSuggest.enablePlayerSuggestions
