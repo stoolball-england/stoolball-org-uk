125ad8347635b0a4b510574a7a0f3af5c19139b0
Rick Mason <caterwomtious@gmail.com>
2020-09-20 20:03:43 +0100

    Add support for Umbraco Forms (only text field tested so far) #135

---
+++ Stoolball.Web/Forms/Form.cs
@@ -0,0 +1,13 @@
+﻿using Stoolball.Metadata;
+using Stoolball.Web.Metadata;
+
+namespace Umbraco.Web.PublishedModels
+{
+    public partial class Form : IHasViewMetadata
+    {
+        /// <summary>
+        /// Gets the metadata for a view
+        /// </summary>
+        public ViewMetadata Metadata { get; set; } = new ViewMetadata();
+    }
+}
