7a32dc4cd900067f7109d746f78c6a384781e14f
Rick Mason <caterwomtious@gmail.com>
2020-09-24 16:59:06 +0100

    Create a content page document type using the block list editor. #55

---
+++ Stoolball.Web/ContentPage/ContentController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Stoolball.Metadata;
+using Stoolball.Web.Security;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.PublishedModels;
+
+namespace Stoolball.Web.ContentPage
+{
+    public class ContentController : RenderMvcController
+    {
+        [HttpGet]
+        [ContentSecurityPolicy]
+        public override ActionResult Index(ContentModel contentModel)
+        {
+            var model = new Content(contentModel?.Content)
+            {
+                Metadata = new ViewMetadata { PageTitle = contentModel.Content.Name }
+            };
+
+            return CurrentTemplate(model);
+        }
+    }
+}
