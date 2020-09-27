86b4b1476f49d3ac45b39a424db72c4e18a58a8e
Rick Mason <caterwomtious@gmail.com>
2020-09-27 21:39:50 +0100

    Add image blocks for content pages #55

---
+++ Stoolball.Web/App_Plugins/Stoolball.BlockEditors/BlockWithImage.controller.js
@@ -0,0 +1,12 @@
+﻿angular.module("umbraco").controller("BlockWithImageController", function ($scope, mediaResource) {
+
+    const imageUdi = $scope.block.data.image;
+    if (imageUdi) {
+        mediaResource.getById(imageUdi)
+            .then(function (media) {
+                $scope.image = media;
+            });
+    }
+
+    $scope.imagePosition = ($scope.block.data.imagePosition && $scope.block.data.imagePosition.toLowerCase()) || "left";
+});
