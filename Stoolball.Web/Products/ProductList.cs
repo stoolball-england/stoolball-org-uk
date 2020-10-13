b59799489bda15acd96ecbb4d90a68786e43de49
Rick Mason <caterwomtious@gmail.com>
2020-10-13 19:25:30 +0100

    Recreate the 'buy' page with a new template and document type #15

---
+++ Stoolball.Web/Products/ProductList.cs
@@ -0,0 +1,19 @@
+﻿using Stoolball.Metadata;
+using Stoolball.Web.Metadata;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Umbraco.Web.PublishedModels
+{
+    public partial class ProductList : IHasViewMetadata
+    {
+        /// <summary>
+        /// Gets the metadata for a view
+        /// </summary>
+        public ViewMetadata Metadata { get; set; } = new ViewMetadata();
+
+        /// <summary>
+        /// Gets the photo that appears in the header of the site
+        /// </summary>
+        public IPublishedContent HeaderPhotoWithInheritance() => this.Value("headerPhoto", fallback: Fallback.ToAncestors) as IPublishedContent;
+    }
+}
---
+++ Stoolball.Web/Products/ProductListController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Stoolball.Metadata;
+using Stoolball.Web.Security;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.PublishedModels;
+
+namespace Stoolball.Web.ContentPage
+{
+    public class ProductListController : RenderMvcController
+    {
+        [HttpGet]
+        [ContentSecurityPolicy]
+        public override ActionResult Index(ContentModel contentModel)
+        {
+            var model = new ProductList(contentModel?.Content);
+            model.Metadata = new ViewMetadata
+            {
+                PageTitle = model.Name,
+                Description = model.Description
+            };
+
+            return CurrentTemplate(model);
+        }
+    }
+}
