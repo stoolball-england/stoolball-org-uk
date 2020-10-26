a1ad41a147bfa3b9979a76cbeb1123a415aa45f9
Rick Mason <caterwomtious@gmail.com>
2020-10-26 21:34:56 +0000

    First draft of a cookie banner #4

--- Stoolball.Web/Security/ContentSecurityPolicyAttribute.cs
+++ Stoolball.Web/Security/ContentSecurityPolicyAttribute.cs
@@ -153,6 +153,7 @@ namespace Stoolball.Web.Security
             AddSource(SCRIPT_SRC, "'self'");
             AddSource(IMG_SRC, "'self'");
             AddSource(IMG_SRC, "https://s.gravatar.com/avatar/");
+            AddSource(IMG_SRC, "data:"); // for Bootstrap custom checkboxes in the cookie banner
             AddSource(CONNECT_SRC, "'self'");
             AddSource(MANIFEST_SRC, "'self'");
         }
---
+++ Stoolball.Web/js/opt-in.js
@@ -0,0 +1,126 @@
+﻿(function () {
+  if (
+    localStorage.getItem("functional_consent") &&
+    localStorage.getItem("improvement_consent") &&
+    localStorage.getItem("tracking_consent")
+  ) {
+    return;
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const optIn = document.createElement("form");
+    optIn.setAttribute("class", "opt-in container-xl");
+    optIn.appendChild(
+      document.createTextNode(
+        "Some features of this site store information on your computer. Is that OK?"
+      )
+    );
+
+    const acceptButton = createButton(
+      "btn btn-primary",
+      "Accept all and close"
+    );
+    const acceptSelectedButton = createButton(
+      "btn btn-primary d-none",
+      "Accept selected"
+    );
+    const rejectButton = createButton("btn btn-secondary", "Reject all");
+    const choicesButton = createButton("btn btn-secondary", "Show choices");
+
+    const choices = document.createElement("div");
+    choices.setAttribute("class", "d-none");
+
+    const essential = createCheckbox(
+      "essential",
+      "Always on (essential features like signing in)"
+    );
+    essential.querySelector("input").setAttribute("checked", "checked");
+    essential.querySelector("input").setAttribute("disabled", "disabled");
+
+    const functional = createCheckbox(
+      "functional",
+      "Optional features (make this site work better, with no tracking)"
+    );
+    const improvement = createCheckbox(
+      "improvement",
+      "Help to improve this site with anonymous statistics (Google Analytics)"
+    );
+    const tracking = createCheckbox(
+      "tracking",
+      "Social media (show better links to services like Facebook and Twitter, but they will track you)"
+    );
+
+    acceptButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      saveChoices(true, true, true);
+      optIn.classList.add("d-none");
+    });
+
+    acceptSelectedButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      saveChoices(
+        functional.querySelector("input").checked,
+        improvement.querySelector("input").checked,
+        tracking.querySelector("input").checked
+      );
+      optIn.classList.add("d-none");
+    });
+
+    choicesButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      choices.setAttribute("class", "d-block");
+      acceptSelectedButton.classList.remove("d-none");
+      acceptSelectedButton.classList.add("d-inline");
+      choicesButton.classList.add("d-none");
+    });
+
+    rejectButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      saveChoices(false, false, false);
+      optIn.classList.add("d-none");
+    });
+
+    choices.appendChild(essential);
+    choices.appendChild(functional);
+    choices.appendChild(improvement);
+    choices.appendChild(tracking);
+
+    optIn.appendChild(choices);
+
+    optIn.appendChild(acceptButton);
+    optIn.appendChild(acceptSelectedButton);
+    optIn.appendChild(rejectButton);
+    optIn.appendChild(choicesButton);
+
+    document.body.insertBefore(optIn, document.body.firstChild);
+  });
+
+  function createButton(className, label) {
+    const button = document.createElement("button");
+    button.setAttribute("class", className);
+    button.appendChild(document.createTextNode(label));
+    return button;
+  }
+
+  function createCheckbox(id, label) {
+    const container = document.createElement("div");
+    container.setAttribute("class", "custom-control custom-checkbox");
+    const input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("class", "custom-control-input");
+    input.setAttribute("id", id);
+    const inputLabel = document.createElement("label");
+    inputLabel.setAttribute("for", id);
+    inputLabel.setAttribute("class", "custom-control-label");
+    inputLabel.appendChild(document.createTextNode(label));
+    container.appendChild(input);
+    container.appendChild(inputLabel);
+    return container;
+  }
+
+  function saveChoices(functional, improvement, tracking) {
+    localStorage.setItem("functional_consent", functional);
+    localStorage.setItem("improvement_consent", improvement);
+    localStorage.setItem("tracking_consent", tracking);
+  }
+})();
