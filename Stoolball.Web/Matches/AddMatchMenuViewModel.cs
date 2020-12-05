d938fa035c6b43bb2d76c8edc91281c82a46dfe6
Rick Mason <caterwomtious@gmail.com>
2020-12-05 17:36:35 +0000

    Update add/edit options to be distinct from tabs, deemphasised relative to tabs, and responsive #363

---
+++ Stoolball.Web/Matches/AddMatchMenuViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Stoolball.Matches;
+
+namespace Stoolball.Web.Matches
+{
+    [ExcludeFromCodeCoverage]
+    public class AddMatchMenuViewModel
+    {
+        public List<MatchType> MatchTypes { get; private set; } = new List<MatchType>();
+        public string BaseRoute { get; set; }
+        public bool EnableTournaments { get; set; }
+    }
+}
