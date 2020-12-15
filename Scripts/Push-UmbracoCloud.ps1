bc9608d2c1d24809f4d882f9ec85e98ddc960b5c
Rick Mason <caterwomtious@gmail.com>
2020-12-15 22:10:07 +0000

    Generate a new ClientDependency version with each deploy

--- Scripts/Push-UmbracoCloud.ps1
+++ Scripts/Push-UmbracoCloud.ps1
@@ -4,6 +4,12 @@ Push-Location $projectRoot
 # Copy changes from Stoolball.Web to the .UmbracoCloud deployment repository
 robocopy .\Stoolball.Web .\.UmbracoCloud /IF *.dll *.cshtml *.uda *.xdt.config *.css *.html *.js package.manifest en-*.xml *.png *.gif *.jpg *.svg /XF Umbraco.*.dll uSync8.*.dll member-group__*.uda *.local.xdt.config /S /XD .git $projectRoot\Stoolball.Web\obj $projectRoot\Stoolball.Web\umbraco $projectRoot\Stoolball.Web\App_Data $projectRoot\Stoolball.Web\App_Plugins\Deploy $projectRoot\Stoolball.Web\App_Plugins\UmbracoForms $projectRoot\Stoolball.Web\App_Plugins\uSync8 $projectRoot\Stoolball.Web\Content $projectRoot\Stoolball.Web\Media
 
+# Update ClientDependency.config version
+$xml = [xml](Select-Xml -Path "$projectRoot\.UmbracoCloud\config\ClientDependency.config" -XPath /).Node; 
+$node = $xml.SelectSingleNode("/clientDependency")
+$node.SetAttribute("version", (Get-Random).ToString())
+$xml.Save("$projectRoot\.UmbracoCloud\config\ClientDependency.config")
+
 # Commit and push those changes
 Push-Location .\.UmbracoCloud
 git add .
