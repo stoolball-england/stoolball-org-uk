16acfd4847fd658752591997f2f6c36698213833
Rick Mason <caterwomtious@gmail.com>
2020-12-15 21:53:31 +0000

    Revise Pull-UmbracoCloud.ps1 to be non-destructive, removing support for non-recommended remote-to-local workflow.

--- Scripts/Pull-UmbracoCloud.ps1
+++ Scripts/Pull-UmbracoCloud.ps1
@@ -1,25 +1,5 @@
-# Ensure the main repository is on the master branch
+# Go and get the latest changes from Umbraco Cloud 
 $projectRoot = Resolve-Path "$PSScriptRoot\.."
-Push-Location $projectRoot
-git checkout master
-
-# Go and get the latest changes from Umbraco Cloud and copy them into the Stoolball.Web
 Push-Location $projectRoot\.UmbracoCloud
 git pull
-robocopy . ..\Stoolball.Web /IF *.uda *.cshtml /XF member-group__*.uda /S /XD .git
 Pop-Location
-
-# Trigger an update from remote to local by Umbraco deploy
-if (!(Test-Path "./Stoolball.Web/data/deploy" -PathType Leaf)) {
-    New-Item -Path "./Stoolball.Web/data" -Name "deploy" -ItemType "file"
-}
-
-Write-Host "THIS COULD LOSE YOUR WORK.
-
-Review the git status below and commit changes to master if appropriate.
-master will then be up-to-date with Umbraco Cloud." -ForegroundColor Red
-git add .
-git status
-
-Pop-Location
-
