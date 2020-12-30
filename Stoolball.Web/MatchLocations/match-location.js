1e2af8a0c1cd63ed15b9971279e439fa4b5ae28d
Rick Mason <caterwomtious@gmail.com>
2020-12-30 20:21:30 +0000

    Review the design of the match location page and ensure it respects privacy choices. Closes #211.

--- Stoolball.Web/MatchLocations/match-location.js
+++ Stoolball.Web/MatchLocations/match-location.js
@@ -1,24 +1,16 @@
 ﻿window.addEventListener("DOMContentLoaded", function (event) {
+  // Check for consent because Google might track the user
+  if (!stoolball.consent.hasTrackingConsent()) {
+    return;
+  }
+
   // Get map placeholder
   const mapContainer = document.getElementById("location-map");
   if (mapContainer) {
     // How accurate?
-    let exactMessage = "";
     let zoomLevel = 14;
-    switch (mapContainer.getAttribute("data-precision")) {
-      case "postcode":
-        exactMessage =
-          "Note: This map shows the nearest postcode. The match location should be nearby.";
-        break;
-      case "street":
-        exactMessage =
-          "Note: This map shows the nearest road. The match location should be nearby.";
-        break;
-      case "town":
-        exactMessage =
-          "Note: This map shows the town or village. Contact the home team to find the match location.";
-        zoomLevel = 11;
-        break;
+    if (mapContainer.getAttribute("data-precision") === "town") {
+      zoomLevel = 11;
     }
 
     // Make the placeholder big enough for a map
@@ -26,6 +18,7 @@
     mapControl.tagName = "div";
     mapControl.style.width = "100%";
     mapControl.style.height = "500px";
+    mapControl.classList.add("print__no-urls");
 
     // Create the map
     const latlng = new google.maps.LatLng(
@@ -46,23 +39,5 @@
       title: document.getElementsByTagName("h1")[0].innerText,
       zIndex: 1,
     });
-
-    // Add a heading to introduce the map
-    // Use DOMParser to safely HTMLDecode the attribute value
-    const mapHeading = document.createElement("h2");
-    let title = new DOMParser().parseFromString(
-      mapContainer.getAttribute("data-title"),
-      "text/html"
-    );
-    mapHeading.appendChild(
-      document.createTextNode("Map of " + title.documentElement.textContent)
-    );
-    mapContainer.parentNode.insertBefore(mapHeading, mapContainer);
-
-    if (exactMessage) {
-      const exactInfo = document.createElement("p");
-      exactInfo.appendChild(document.createTextNode(exactMessage));
-      mapContainer.parentNode.insertBefore(exactInfo, mapContainer);
-    }
   }
 });
