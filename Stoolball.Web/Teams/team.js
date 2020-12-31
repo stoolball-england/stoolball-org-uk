9cea2d48474630763ecc7b5f740fc4656dcc4386
Rick Mason <caterwomtious@gmail.com>
2020-12-31 18:26:29 +0000

    Change the season list on team pages to show only the most recent season(s) by default rather than all seasons, because some teams now have far too many that dominate the page and are distant history. #213

---
+++ Stoolball.Web/Teams/team.js
@@ -0,0 +1,30 @@
+﻿window.addEventListener("DOMContentLoaded", function (event) {
+  // If there's a complete list of competitions hidden on initial load, add a button to show it instead of the shorter list
+  const allCompetitions = document.querySelector(".season-list__all");
+  if (allCompetitions) {
+    const showAllCompetitions = document.createElement("button");
+    showAllCompetitions.setAttribute("type", "button");
+    showAllCompetitions.appendChild(
+      document.createTextNode("Show all competitions")
+    );
+    showAllCompetitions.classList.add("btn");
+    showAllCompetitions.classList.add("btn-secondary");
+    showAllCompetitions.classList.add("d-print-none");
+    showAllCompetitions.classList.add("season-list__show-all");
+
+    showAllCompetitions.addEventListener("click", function () {
+      allCompetitions.classList.remove("d-none");
+      showAllCompetitions.parentNode.removeChild(showAllCompetitions);
+
+      const currentSeasons = document.querySelector(".season-list__current");
+      if (currentSeasons) {
+        currentSeasons.parentNode.removeChild(currentSeasons);
+      }
+    });
+
+    allCompetitions.parentNode.insertBefore(
+      showAllCompetitions,
+      allCompetitions
+    );
+  }
+});
