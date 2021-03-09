eb815ee82c9bdd3f923073a991cb68fcbc52d3c7
Rick Mason <caterwomtious@gmail.com>
2021-03-09 09:51:28 +0000

    Create a handler for .csv URLs which can return a generated file, and return a different version of that file if a valid API key is in the URL. Replicates the behaviour of the old website for #39 and #40.

---
+++ Stoolball.Web/WebApi/CsvHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Stoolball.Web.WebApi
+{
+    public class CsvHandler : IHttpHandler
+    {
+        public bool IsReusable {
+            // Return false in case your Managed Handler cannot be reused for another request.
+            // Usually this would be false in case you have some state information preserved per request.
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context is null)
+            {
+                throw new System.ArgumentNullException(nameof(context));
+            }
+
+            var requestedPath = Path.GetDirectoryName(context.Request.Url.AbsolutePath);
+            if (requestedPath.ToUpperInvariant() != @"\DATA")
+            {
+                context.Response.StatusCode = 404;
+                return;
+            }
+
+            var requestedFile = Path.GetFileName(context.Request.Url.AbsolutePath);
+            var path = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, @"App_Data\csv", requestedFile);
+            if (!File.Exists(path))
+            {
+                context.Response.StatusCode = 404;
+            }
+
+            if (HasValidApiKey(context.Request.QueryString["key"]))
+            {
+                var extension = Path.GetExtension(requestedFile);
+                var protectedPath = path.Substring(0, path.Length - extension.Length) + "-protected" + extension;
+                if (File.Exists(protectedPath))
+                {
+                    path = protectedPath;
+                }
+            }
+
+            context.Response.WriteFile(path);
+        }
+
+        private static bool HasValidApiKey(string apiKey)
+        {
+            if (string.IsNullOrEmpty(apiKey)) { return false; }
+
+            var validApiKeys = ConfigurationManager.GetSection("StoolballApiKeys") as NameValueCollection;
+            foreach (var key in validApiKeys.AllKeys)
+            {
+                if (validApiKeys[key] == apiKey) { return true; }
+            }
+            return false;
+        }
+    }
+}
