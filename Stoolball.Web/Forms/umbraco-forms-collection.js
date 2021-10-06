2c1c8e1b84c7a526e70a401d051cc10a642569d4
Rick Mason <caterwomtious@gmail.com>
2021-10-06 14:02:22 +0100

    Fix Umbraco Forms error in Firefox on the shop page #468

--- Stoolball.Web/Forms/umbraco-forms-collection.js
+++ Stoolball.Web/Forms/umbraco-forms-collection.js
@@ -1,12 +1,7 @@
 ﻿// deliberately using var to be in global scope and running immediately - out-of-the-box this is inline script
-const umbracoFormsCollectionData = document.getElementById(
-  "umbraco-forms-collection"
-);
-if (umbracoFormsCollectionData) {
-  if (typeof umbracoFormsCollection === "undefined") {
-    var umbracoFormsCollection = [];
-  }
-  umbracoFormsCollection.push(
-    umbracoFormsCollectionData.getAttribute("data-forms")
-  );
+if (typeof umbracoFormsCollection === "undefined") {
+  var umbracoFormsCollection = [];
 }
+umbracoFormsCollection.push(
+  document.getElementById("umbraco-forms-collection").getAttribute("data-forms")
+);
