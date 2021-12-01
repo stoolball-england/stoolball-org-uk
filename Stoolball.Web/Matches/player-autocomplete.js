b62cfd6eec8b5984085f0d7d507d2ceefa6abf00
Rick Mason <caterwomtious@gmail.com>
2021-12-01 19:06:15 +0000

    Player lookups are too slow. Refactor away a DOM manipulation.

--- Stoolball.Web/Matches/player-autocomplete.js
+++ Stoolball.Web/Matches/player-autocomplete.js
@@ -9,18 +9,17 @@ stoolball.autocompletePlayer = function (input) {
     stoolball.autocompletePlayer.cachedSuggestions = [];
   }
 
-  function cacheSuggestedPlayer(teamId, suggestion) {
-    if (!stoolball.autocompletePlayer.cachedSuggestions[teamId]) {
-      stoolball.autocompletePlayer.cachedSuggestions[teamId] = [];
+  // Cache new players first mentioned on this page
+  if (!stoolball.autocompletePlayer.newPlayers) {
+    stoolball.autocompletePlayer.newPlayers = [];
+  }
+
+  function cacheValueByTeam(cache, teamId, cachedValue) {
+    if (!cache[teamId]) {
+      cache[teamId] = [];
     }
-    if (
-      stoolball.autocompletePlayer.cachedSuggestions[teamId].indexOf(
-        suggestion.value
-      ) === -1
-    ) {
-      stoolball.autocompletePlayer.cachedSuggestions[teamId].push(
-        suggestion.value
-      );
+    if (cache[teamId].indexOf(cachedValue) === -1) {
+      cache[teamId].push(cachedValue);
     }
   }
 
@@ -59,7 +58,11 @@ stoolball.autocompletePlayer = function (input) {
       // Cache the suggested value so that we can check on blur whether the entered value was one of the suggestions.
       const targetTeam = input.getAttribute("data-team");
       if (suggestion.data.playerRecord !== thisMatch) {
-        cacheSuggestedPlayer(targetTeam, suggestion);
+        cacheValueByTeam(
+          stoolball.autocompletePlayer.cachedSuggestions,
+          targetTeam,
+          suggestion.value
+        );
       }
       return (
         $.Autocomplete.defaults.formatResult(suggestion, currentValue) +
@@ -71,24 +74,17 @@ stoolball.autocompletePlayer = function (input) {
     transformResult: function (response, originalQuery) {
       response = JSON.parse(response);
 
-      // querySelectorAll to get new players, and slice to convert that to an array.
-      // map to get the input.value from the input, and filter to get unique values,
-      // then filter again to get values matching the search term.
-      const newPlayerSuggestions = [].slice
-        .call(
-          document.querySelectorAll(
-            ".scorecard__player-name[data-new-player='true']"
-          )
-        )
-        .map(function (x) {
-          return x.value;
-        })
-        .filter(function (value, index, self) {
-          return self.indexOf(value.trim()) === index;
-        })
-        .filter(function (value) {
-          return RegExp(originalQuery.replace(/\s/, ".*"), "gi").test(value);
-        });
+      // Find new players first mentioned on this page, who match the search term.
+      const targetTeam = input.getAttribute("data-team");
+      const newPlayerSuggestions = stoolball.autocompletePlayer.newPlayers[
+        targetTeam
+      ]
+        ? stoolball.autocompletePlayer.newPlayers[targetTeam].filter(function (
+            value
+          ) {
+            return RegExp(originalQuery.replace(/\s/, ".*"), "gi").test(value);
+          })
+        : [];
 
       // Add any matching new players to the start of the suggestions list.
       for (let i = 0; i < newPlayerSuggestions.length; i++) {
@@ -103,13 +99,17 @@ stoolball.autocompletePlayer = function (input) {
     },
   });
 
-  // When the field blurs a name has been chosen, so set an attribute to record whether it's a new player
+  // When the field blurs a name has been chosen, so add to an internal cache if it's a new player
   input.addEventListener("blur", function (e) {
-    this.value = capitalise(this.value.trim());
-    this.setAttribute(
-      "data-new-player",
-      !isSuggestedPlayer(e.target.getAttribute("data-team"), this.value)
-    );
+    const playerName = capitalise(this.value.trim());
+    const teamId = e.target.getAttribute("data-team");
+    if (!isSuggestedPlayer(teamId, this.value)) {
+      cacheValueByTeam(
+        stoolball.autocompletePlayer.newPlayers,
+        teamId,
+        playerName
+      );
+    }
   });
 };
 
