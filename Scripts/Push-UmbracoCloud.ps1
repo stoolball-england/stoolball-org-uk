07f7419b5bb81a0d49c925340ec9c2f24b233092
Rick Mason <caterwomtious@gmail.com>
2022-01-16 23:06:23 +0000

    v9: Update push script

--- Scripts/Push-UmbracoCloud.ps1
+++ Scripts/Push-UmbracoCloud.ps1
@@ -1,31 +1,41 @@
 $projectRoot = Resolve-Path "$PSScriptRoot\.."
-Push-Location $projectRoot
 
-# Pull updates from remote first - it should avoid merge conflicts with the commit which happens below
-Push-Location .\.UmbracoCloud
-git pull origin master
-Pop-Location
-
-# Copy changes from Stoolball.Web to the .UmbracoCloud deployment repository
-robocopy .\Stoolball.Web .\.UmbracoCloud /IF *.dll *.cshtml *.uda *.xdt.config *.css *.html *.js package.manifest en-*.xml *.png *.gif *.jpg *.svg *.ico *.woff *.woff2 /XF Umbraco.*.dll uSync8.*.dll member-group__*.uda *.local.xdt.config *.template.config *.test.js /S /XD .git $projectRoot\Stoolball.Web\obj $projectRoot\Stoolball.Web\umbraco $projectRoot\Stoolball.Web\App_Data $projectRoot\Stoolball.Web\App_Plugins\Deploy $projectRoot\Stoolball.Web\App_Plugins\UmbracoForms $projectRoot\Stoolball.Web\App_Plugins\uSync8 $projectRoot\Stoolball.Web\Content $projectRoot\Stoolball.Web\Media
-copy .\Stoolball.Web\App_Plugins\UmbracoForms\Backoffice\Common\FieldTypes\EmailField.html .\.UmbracoCloud\App_Plugins\UmbracoForms\Backoffice\Common\FieldTypes
-copy .\Stoolball.Web\ApplicationInsights.template.config .\.UmbracoCloud\ApplicationInsights.config
-copy .\Stoolball.Web\fonts\Web.config .\.UmbracoCloud\fonts
-copy .\Stoolball.Web\images\Web.config .\.UmbracoCloud\images
-
-# Update ClientDependency.config and service worker version
-$version = (Get-Random).ToString();
-$xml = [xml](Select-Xml -Path "$projectRoot\.UmbracoCloud\config\ClientDependency.config" -XPath /).Node; 
-$node = $xml.SelectSingleNode("/clientDependency")
-$node.SetAttribute("version", $version)
-$xml.Save("$projectRoot\.UmbracoCloud\config\ClientDependency.config")
-(Get-Content "$projectRoot\.UmbracoCloud\service-worker.js") -Replace "version = '1.0.0'", "version = '$version'" | Set-Content "$projectRoot\.UmbracoCloud\service-worker.js"
-
-# Commit and push those changes
-Push-Location .\.UmbracoCloud
-git add .
-git commit -am "Changes from local site"
-git push origin master
-Pop-Location
-
-Pop-Location
+# Get the path to the Umbraco code from the .umbraco Umbraco Cloud config file
+$projectConfig = Get-Content $projectRoot/.umbraco
+$configLine = $projectConfig | Where-Object { $_.StartsWith("base") } | Select-Object -First 1
+if ($configLine -match 'base = "(?<UmbracoRoot>src/[\w.-]+)"') {
+    $umbracoProjectPath = $Matches.UmbracoRoot
+    $umbracoRoot = Resolve-Path "$projectRoot\Stoolball.Web"
+    $cloudRoot = Resolve-Path "$projectRoot\.UmbracoCloud"
+    
+    # Pull updates from remote first - it should avoid merge conflicts with the commit which happens below
+    Push-Location $cloudRoot
+    git pull origin master
+    Pop-Location
+    
+    # Build the code - it gets built anyway during deploy but this validates it builds before pushing, and will execute any MSBuild steps
+    Push-Location $umbracoRoot
+    dotnet build
+    Pop-Location
+    
+    # Copy changes from $umbracoRoot to the .UmbracoCloud deployment repository
+    robocopy $umbracoRoot $cloudRoot\$umbracoProjectPath `
+        /IF *.cs *.csproj *.cshtml *.uda *.config *.css *.html *.js package.manifest en-*.xml *.png *.gif *.jpg *.svg *.ico *.woff *.woff2 appsettings.json `
+        /XF Umbraco.*.dll uSync*.dll member-group__*.uda *.test.js `
+        /S `
+        /XD .git $umbracoRoot\obj $umbracoRoot\bin $umbracoRoot\node_modules $umbracoRoot\sass $umbracoRoot\App_Data $umbracoRoot\wwwroot\media $umbracoRoot\wwwroot\umbraco `
+        $umbracoRoot\App_Plugins\Deploy $umbracoRoot\App_Plugins\UmbracoForms $umbracoRoot\App_Plugins\UmbracoId $umbracoRoot\App_Plugins\uSync8 `
+        $umbracoRoot\umbraco\config $umbracoRoot\umbraco\Data $umbracoRoot\umbraco\config\Logs $umbracoRoot\umbraco\PartialViewMacros $umbracoRoot\umbraco\UmbracoBackOffice `
+        $umbracoRoot\umbraco\UmbracoInstall $umbracoRoot\umbraco\UmbracoWebsite
+    
+    # Update versions for each deploy where needed
+    # $version = (Get-Random).ToString();
+    # (Get-Content "$cloudRoot\$umbracoProjectPath\wwwroot\service-worker.js") -Replace "version = '1.0.0'", "version = '$version'" | Set-Content "$cloudRoot\$umbracoProjectPath\wwwroot\service-worker.js"
+    
+    # Commit and push those changes
+    Push-Location $cloudRoot
+    git add .
+    git commit -am "Changes from local site"
+    git push origin master
+    Pop-Location
+}
