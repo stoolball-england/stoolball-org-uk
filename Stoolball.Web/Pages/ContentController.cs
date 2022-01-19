66d38bf05c70a0f8ecf63f18a0d529b6aa85b000
Rick Mason <caterwomtious@gmail.com>
2022-01-19 15:12:59 +0000

    v9: Migrate the content page template

---
+++ Stoolball.Web/Pages/ContentController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Stoolball.Metadata;
+using Stoolball.Web.Models;
+using Stoolball.Web.Security;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Stoolball.Web.Pages
+{
+    public class ContentController : RenderController
+    {
+        private readonly IVariationContextAccessor _variationContextAccessor;
+        private readonly ServiceContext _serviceContext;
+
+        public ContentController(ILogger<ContentController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor, IVariationContextAccessor variationContextAccessor, ServiceContext context)
+            : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _variationContextAccessor = variationContextAccessor;
+            _serviceContext = context;
+        }
+
+        [HttpGet]
+        [ContentSecurityPolicy(Forms = true, TinyMCE = true, GettyImages = true, YouTube = true)]
+        public override IActionResult Index()
+        {
+            var model = new Content(CurrentPage, new PublishedValueFallback(_serviceContext, _variationContextAccessor));
+            model.Metadata = new ViewMetadata
+            {
+                PageTitle = model.Name,
+                Description = model.Description
+            };
+
+            return CurrentTemplate(model);
+        }
+
+        ///* This action is triggered when an Umbraco Forms form is submitted without a separate 'thank you' page */
+        //[HttpPost]
+        //public IActionResult Content()
+        //{
+        //    var model = new Content(CurrentPage, new PublishedValueFallback(_serviceContext, _variationContextAccessor));
+        //    model.Metadata = new ViewMetadata
+        //    {
+        //        PageTitle = model.Name,
+        //        Description = model.Description
+        //    };
+
+        //    return CurrentTemplate(model);
+        //}
+    }
+}
