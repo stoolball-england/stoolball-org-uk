2188899df1094adf9a656895aaa5412d2b47dcd6
Rick Mason <caterwomtious@gmail.com>
2022-01-19 00:17:51 +0000

    v9: Update deployment script to handle project references

--- Scripts/Push-UmbracoCloud.ps1
+++ Scripts/Push-UmbracoCloud.ps1
@@ -1,41 +1,58 @@
 $projectRoot = Resolve-Path "$PSScriptRoot\.."
+$cloudRoot = Resolve-Path "$projectRoot\.UmbracoCloud"
 
-# Get the path to the Umbraco code from the .umbraco Umbraco Cloud config file
+$foldersToCopy = "Stoolball", "Stoolball.Web"
+
+# Get the path to the Umbraco project from the .umbraco Umbraco Cloud config file
 $projectConfig = Get-Content $projectRoot/.umbraco
-$configLine = $projectConfig | Where-Object { $_.StartsWith("base") } | Select-Object -First 1
-if ($configLine -match 'base = "(?<UmbracoRoot>src/[\w.-]+)"') {
-    $umbracoProjectPath = $Matches.UmbracoRoot
-    $umbracoRoot = Resolve-Path "$projectRoot\Stoolball.Web"
-    $cloudRoot = Resolve-Path "$projectRoot\.UmbracoCloud"
-    
-    # Pull updates from remote first - it should avoid merge conflicts with the commit which happens below
-    Push-Location $cloudRoot
-    git pull origin master
-    Pop-Location
-    
-    # Build the code - it gets built anyway during deploy but this validates it builds before pushing, and will execute any MSBuild steps
-    Push-Location $umbracoRoot
-    dotnet build
-    Pop-Location
-    
-    # Copy changes from $umbracoRoot to the .UmbracoCloud deployment repository
-    robocopy $umbracoRoot $cloudRoot\$umbracoProjectPath `
-        /IF *.cs *.csproj *.cshtml *.uda *.css *.html *.js package.manifest en-*.xml *.png *.gif *.jpg *.svg *.ico *.woff *.woff2 appsettings.json `
-        /XF Umbraco.*.dll uSync*.dll member-group__*.uda *.test.js `
-        /S `
-        /XD .git $umbracoRoot\obj $umbracoRoot\bin $umbracoRoot\node_modules $umbracoRoot\sass $umbracoRoot\App_Data $umbracoRoot\wwwroot\media $umbracoRoot\wwwroot\umbraco `
-        $umbracoRoot\App_Plugins\Deploy $umbracoRoot\App_Plugins\UmbracoForms $umbracoRoot\App_Plugins\UmbracoId $umbracoRoot\App_Plugins\uSync `
-        $umbracoRoot\umbraco\config $umbracoRoot\umbraco\Data $umbracoRoot\umbraco\config\Logs $umbracoRoot\umbraco\PartialViewMacros $umbracoRoot\umbraco\UmbracoBackOffice `
-        $umbracoRoot\umbraco\UmbracoInstall $umbracoRoot\umbraco\UmbracoWebsite
-    
-    # Update versions for each deploy where needed
-    # $version = (Get-Random).ToString();
-    # (Get-Content "$cloudRoot\$umbracoProjectPath\wwwroot\service-worker.js") -Replace "version = '1.0.0'", "version = '$version'" | Set-Content "$cloudRoot\$umbracoProjectPath\wwwroot\service-worker.js"
-    
-    # Commit and push those changes
-    Push-Location $cloudRoot
-    git add .
-    git commit -am "Changes from local site"
-    git push origin master
-    Pop-Location
+$configLine = $projectConfig | Where-Object { $_.StartsWith("csproj") } | Select-Object -First 1
+if ($configLine -match 'csproj = "(?<csproj>[\w.-]+)"') {
+    $umbracoProjectFile = $Matches.csproj
 }
+
+
+# Pull updates from remote first - it should avoid merge conflicts with the commit which happens below
+Push-Location $cloudRoot
+git pull origin master
+Pop-Location
+
+# Build the code - it gets built anyway during deploy but this validates it builds before pushing, and will execute any MSBuild steps
+foreach ($folder in $foldersToCopy) {
+    $folderPath = Resolve-Path (Join-Path $projectRoot -ChildPath $folder)
+    if (Test-Path (Join-Path $folderPath -ChildPath $umbracoProjectFile)) {
+        Push-Location $folderPath
+        dotnet build
+        Pop-Location
+        break
+    }
+}
+
+# Copy changes to the .UmbracoCloud deployment repository
+Copy-Item $projectRoot\.umbraco $cloudRoot
+foreach ($folder in $foldersToCopy) {
+    $folderPath = Resolve-Path (Join-Path $projectRoot -ChildPath $folder)
+
+    robocopy $folderPath $cloudRoot\src\$folder `
+        /IF *.cs *.csproj *.cshtml *.uda *.css *.html *.js package.manifest en-*.xml *.png *.gif *.jpg *.svg *.ico *.woff *.woff2 appsettings.json compilerconfig.json `
+        /XF member-group__*.uda *.test.js `
+        /S `
+        /XD $folderPath\obj $folderPath\bin $folderPath\node_modules $folderPath\sass $folderPath\App_Data $folderPath\wwwroot\media $folderPath\wwwroot\umbraco `
+        $folderPath\App_Plugins\Deploy $folderPath\App_Plugins\UmbracoForms $folderPath\App_Plugins\UmbracoId $folderPath\App_Plugins\uSync `
+        $folderPath\umbraco\config $folderPath\umbraco\Data $folderPath\umbraco\config\Logs $folderPath\umbraco\PartialViewMacros $folderPath\umbraco\UmbracoBackOffice `
+        $folderPath\umbraco\UmbracoInstall $folderPath\umbraco\UmbracoWebsite
+
+    # Update versions for each deploy where needed
+    $version = (Get-Random).ToString();
+    $targetFile = "$cloudRoot\src\$folder\wwwroot\service-worker.js"
+    if (Test-Path $targetFile) {
+        (Get-Content $targetFile) -Replace "version = '1.0.0'", "version = '$version'" | Set-Content $targetFile
+    }
+}    
+    
+    
+# Commit and push those changes
+Push-Location $cloudRoot
+git add .
+git commit -am "Changes from local site"
+git push origin master
+Pop-Location
