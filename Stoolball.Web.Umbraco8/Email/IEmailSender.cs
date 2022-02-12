cf274805da3e1133dfe7d8137fb6962ea5143bf3
Rick Mason <caterwomtious@gmail.com>
2022-02-12 11:46:10 +0000

    v9: Remove obsolete IEmailSender

--- Stoolball.Web.Umbraco8/Email/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Stoolball.Web.Email
-{
-    public interface IEmailSender
-    {
-        bool SendEmail(string toAddress, string subject, string bodyHtml);
-    }
-}
