ae31345a628654a46d59a7fcda5850152877c7c3
Rick Mason <caterwomtious@gmail.com>
2022-09-17 13:26:25 +0100

    Select a preferred name and display the alternatives for a player with multiple identities #'615

---
+++ Stoolball.UnitTests/Statistics/PlayerTests.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using Stoolball.Statistics;
+using Xunit;
+
+namespace Stoolball.UnitTests.Statistics
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void Player_name_orders_by_total_matches_aggregating_duplicates()
+        {
+            var player = new Player
+            {
+                PlayerIdentities = new List<PlayerIdentity>
+                {
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name One",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name Two",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name Two",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date
+                    }
+                }
+            };
+
+            var preferredName = player.PlayerName();
+            var alternativeNames = player.AlternativeNames();
+
+            Assert.Equal("Name Two", preferredName);
+            Assert.Single(alternativeNames);
+            Assert.Equal("Name One", alternativeNames[0]);
+        }
+
+        [Fact]
+        public void Player_name_prefers_most_recent_player()
+        {
+            var player = new Player
+            {
+                PlayerIdentities = new List<PlayerIdentity>
+                {
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name One",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-1)
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name Two",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date
+                    }
+                }
+            };
+
+            var preferredName = player.PlayerName();
+            var alternativeNames = player.AlternativeNames();
+
+            Assert.Equal("Name Two", preferredName);
+            Assert.Single(alternativeNames);
+            Assert.Equal("Name One", alternativeNames[0]);
+        }
+
+        [Theory]
+        [InlineData("Name Complete")]
+        [InlineData("Name von Complete")]
+        public void Player_name_prefers_complete_names(string completeName)
+        {
+            var player = new Player
+            {
+                PlayerIdentities = new List<PlayerIdentity>
+                {
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name A",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = completeName,
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date
+                    }
+                }
+            };
+
+            var preferredName = player.PlayerName();
+            var alternativeNames = player.AlternativeNames();
+
+            Assert.Equal(completeName, preferredName);
+            Assert.Equal(2, alternativeNames.Count);
+            Assert.Equal("Name A", alternativeNames[0]);
+            Assert.Equal("Name", alternativeNames[1]);
+        }
+
+        [Fact]
+        public void Player_name_orders_each_group_by_total_matches_boosting_recent_players()
+        {
+            var player = new Player
+            {
+                PlayerIdentities = new List<PlayerIdentity>
+                {
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name One",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-5) // Fifth most recent, should boost to 2 matches * 1 weight = 2
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name Two",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-3) // Third most recent, should boost to 2 matches * 3 weight = 6
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name Three",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-1) // Most recent, should boost to 2 matches * 5 weight = 10
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name Four",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-2) // Second most recent, should boost to 2 matches * 4 weight = 8
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name Five",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-4) // Fourth most recent, should boost to 2 matches * 2 weight = 4
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name A",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-5) // Fifth most recent, should boost to 2 matches * 1 weight = 2
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name B",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-3) // Third most recent, should boost to 2 matches * 3 weight = 6
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name C",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-1) // Most recent, should boost to 2 matches * 5 weight = 10
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name D",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-2) // Second most recent, should boost to 2 matches * 4 weight = 8
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "Name E",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-4) // Fourth most recent, should boost to 2 matches * 2 weight = 4
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "NameA",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-5) // Fifth most recent, should boost to 2 matches * 1 weight = 2
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "NameB",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-3) // Third most recent, should boost to 2 matches * 3 weight = 6
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "NameC",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-1) // Most recent, should boost to 2 matches * 5 weight = 10
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "NameD",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-2) // Second most recent, should boost to 2 matches * 4 weight = 8
+                    },
+                    new PlayerIdentity {
+                        PlayerIdentityName = "NameE",
+                        TotalMatches = 2,
+                        LastPlayed = DateTimeOffset.Now.Date.AddDays(-4) // Fourth most recent, should boost to 2 matches * 2 weight = 4
+                    }
+                }
+            };
+
+            var preferredName = player.PlayerName();
+            var alternativeNames = player.AlternativeNames();
+
+            Assert.Equal("Name Three", preferredName);
+            Assert.Equal(14, alternativeNames.Count);
+            Assert.Equal("Name Four", alternativeNames[0]);
+            Assert.Equal("Name Two", alternativeNames[1]);
+            Assert.Equal("Name Five", alternativeNames[2]);
+            Assert.Equal("Name One", alternativeNames[3]);
+            Assert.Equal("Name C", alternativeNames[4]);
+            Assert.Equal("Name D", alternativeNames[5]);
+            Assert.Equal("Name B", alternativeNames[6]);
+            Assert.Equal("Name E", alternativeNames[7]);
+            Assert.Equal("Name A", alternativeNames[8]);
+            Assert.Equal("NameC", alternativeNames[9]);
+            Assert.Equal("NameD", alternativeNames[10]);
+            Assert.Equal("NameB", alternativeNames[11]);
+            Assert.Equal("NameE", alternativeNames[12]);
+            Assert.Equal("NameA", alternativeNames[13]);
+        }
+    }
+}
--- Stoolball/Statistics/Player.cs
+++ Stoolball/Statistics/Player.cs
@@ -7,11 +7,143 @@ namespace Stoolball.Statistics
 {
     public class Player : IAuditable
     {
+        private string _preferredName = string.Empty;
+        private readonly List<string> _alternativeNames = new();
+
         public Guid? PlayerId { get; set; }
 
+        private class CandidateName
+        {
+            public string Name { get; set; }
+            public int TotalMatches { get; set; }
+            public DateTimeOffset? LastPlayed { get; set; }
+            public int Weighting { get; set; } = 1;
+        }
+
+        /// <summary>
+        /// Gets the preferred name for a player based on all the <see cref="PlayerIdentities"/>
+        /// </summary>
         public string PlayerName()
         {
-            return string.Join("/", PlayerIdentities.Select(x => x.PlayerIdentityName).Distinct());
+            InitialisePreferredAndAlternativeNames();
+            return _preferredName;
+        }
+
+        /// <summary>
+        /// Select a preferred name and a list of alternative names from the <see cref="PlayerIdentities"/>
+        /// </summary>
+        private void InitialisePreferredAndAlternativeNames()
+        {
+            if (string.IsNullOrEmpty(_preferredName))
+            {
+                var names = CombineDuplicateNames();
+                var groups = DivideNamesIntoGroups(names);
+                for (var i = 0; i < groups.Count; i++)
+                {
+                    groups[i] = AssignWeightingBasedOnLastPlayed(groups[i]);
+                    groups[i] = OrderByWeightedTotalMatches(groups[i]);
+                }
+                var recombined = groups[0].Concat(groups[1]).Concat(groups[2]).ToList();
+                if (recombined.Count > 0)
+                {
+                    _preferredName = recombined[0].Name;
+                    recombined.RemoveAt(0);
+                }
+                if (recombined.Count > 0)
+                {
+                    _alternativeNames.AddRange(recombined.Select(x => x.Name));
+                }
+            };
+        }
+
+        /// <summary>
+        /// Order a list of names by the total matches played * the weighting
+        /// </summary>
+        /// <param name="group"></param>
+        private List<CandidateName> OrderByWeightedTotalMatches(List<CandidateName> group)
+        {
+            return group.OrderByDescending(x => x.TotalMatches * x.Weighting).ToList();
+        }
+
+        /// <summary>
+        /// Assign the names most recently recorded a higher weighting
+        /// </summary>
+        private List<CandidateName> AssignWeightingBasedOnLastPlayed(List<CandidateName> group)
+        {
+            var recent = group.Where(x => x.LastPlayed.HasValue).OrderByDescending(x => x.LastPlayed).Take(4);
+            var weighting = 6;
+            var lastPlayed = DateTimeOffset.MaxValue;
+            foreach (var candidate in recent)
+            {
+                if (candidate.LastPlayed < lastPlayed)
+                {
+                    weighting--;
+                }
+                candidate.Weighting = weighting;
+                lastPlayed = candidate.LastPlayed.Value;
+            }
+            return group;
+        }
+
+        /// <summary>
+        /// Complete names are better. Divide into three groups - two or more names, one name + initial, one name
+        /// </summary> 
+        private List<List<CandidateName>> DivideNamesIntoGroups(IEnumerable<CandidateName> names)
+        {
+            var group1 = new List<CandidateName>();
+            var group2 = new List<CandidateName>();
+            var group3 = new List<CandidateName>();
+
+            foreach (var name in names)
+            {
+                var firstSpace = name.Name.IndexOf(" ");
+                if (firstSpace > -1)
+                {
+                    var lastSpace = name.Name.LastIndexOf(" ");
+                    if (firstSpace == lastSpace && lastSpace == name.Name.Length - 2)
+                    {
+                        group2.Add(name);
+                    }
+                    else
+                    {
+                        group1.Add(name);
+                    }
+                }
+                else
+                {
+                    group3.Add(name);
+                }
+            }
+
+            return new List<List<CandidateName>> { group1, group2, group3 };
+        }
+
+        /// <summary>
+        /// Combine duplicate names, aggregating total matches and preferring the most recent usage of the name
+        /// </summary>
+        private IEnumerable<CandidateName> CombineDuplicateNames()
+        {
+            var names = new Dictionary<string, CandidateName>();
+            foreach (var identity in PlayerIdentities)
+            {
+                if (string.IsNullOrEmpty(identity.PlayerIdentityName)) { continue; }
+                if (!names.ContainsKey(identity.PlayerIdentityName))
+                {
+                    names.Add(identity.PlayerIdentityName, new CandidateName { Name = identity.PlayerIdentityName, TotalMatches = identity.TotalMatches ?? 0, LastPlayed = identity.LastPlayed });
+                }
+                else
+                {
+                    if (identity.TotalMatches.HasValue)
+                    {
+                        names[identity.PlayerIdentityName].TotalMatches += identity.TotalMatches.Value;
+                    }
+                    if (!names[identity.PlayerIdentityName].LastPlayed.HasValue || (identity.LastPlayed.HasValue && identity.LastPlayed > names[identity.PlayerIdentityName].LastPlayed))
+                    {
+                        names[identity.PlayerIdentityName].LastPlayed = identity.LastPlayed;
+                    }
+                }
+            }
+            return names.Values;
         }
 
         public string PlayerRoute { get; set; }
@@ -24,6 +156,16 @@ namespace Stoolball.Statistics
 
         public Uri EntityUri {
             get { return new Uri($"https://www.stoolball.org.uk/id/player/{PlayerId}"); }
+        }
+
+        /// <summary>
+        /// Gets the alternative names for a player with multiple player identities where the names differ
+        /// </summary>
+        /// <returns>A list of 0 or more names</returns>
+        public List<string> AlternativeNames()
+        {
+            InitialisePreferredAndAlternativeNames();
+            return _alternativeNames;
         }
     }
 }
