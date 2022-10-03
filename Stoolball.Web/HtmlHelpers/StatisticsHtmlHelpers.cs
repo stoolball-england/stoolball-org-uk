1472516b7f6348288e043e452245ae58fcf71d14
Rick Mason <caterwomtious@gmail.com>
2022-10-03 19:55:49 +0100

    When a statistics page was loaded with a trailing / it led to a // in the statistics table links. Fixes #628.

---
+++ Stoolball.Web/HtmlHelpers/StatisticsHtmlHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Umbraco.Extensions;
+
+namespace Stoolball.Web.HtmlHelpers
+{
+    public static class StatisticsHtmlHelpers
+    {
+        public static string LinkToStatisticsTable(this IHtmlHelper html, string statisticsUrlSegment, string? queryString)
+        {
+            return (html.ViewContext.HttpContext.Request.Path.Value ?? string.Empty).TrimEnd("/").TrimEnd("/batting").TrimEnd("/bowling").TrimEnd("/fielding") + "/" + statisticsUrlSegment + queryString;
+        }
+    }
+}
