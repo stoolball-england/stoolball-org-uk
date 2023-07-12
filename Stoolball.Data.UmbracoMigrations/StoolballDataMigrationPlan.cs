b6dee981f5ae88250d4c5a18bf70ab9b32b3b701
Rick Mason <caterwomtious@gmail.com>
2023-07-12 12:12:42 +0100

    Migration plan did not match that applied in production

--- Stoolball.Data.UmbracoMigrations/StoolballDataMigrationPlan.cs
+++ Stoolball.Data.UmbracoMigrations/StoolballDataMigrationPlan.cs
@@ -53,7 +53,7 @@ namespace Stoolball.Data.UmbracoMigrations
                 .To<LinkPlayerToMember>(typeof(LinkPlayerToMember).ToString())
                 .To<LinkPlayerToMemberAsyncUpdate>(typeof(LinkPlayerToMemberAsyncUpdate).ToString())
                 .To<IndexTeamRouteAndOppositionTeamRoute>(typeof(IndexTeamRouteAndOppositionTeamRoute).ToString())
-                .To<AddLinkedByToPlayerIdentity>(typeof(AddLinkedByToPlayerIdentity).ToString());
+                .To<AddRouteSegmentToPlayerIdentity>(typeof(AddRouteSegmentToPlayerIdentity).ToString());
         }
     }
 }
