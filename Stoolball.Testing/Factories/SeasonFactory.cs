9c29624e5194dbf993715bf89e5486df6396524d
Rick Mason <caterwomtious@gmail.com>
2026-09-13 23:08:25 +0100

    Recent change to SeasonFactory was generating an incorrect route that broke tests

--- Stoolball.Testing/Factories/SeasonFactory.cs
+++ Stoolball.Testing/Factories/SeasonFactory.cs
@@ -16,7 +16,9 @@
                     .RuleFor(x => x.Competition, () => competition)
                     .RuleFor(x => x.FromYear, faker => fromYear ?? faker.Random.Int(2000, 2025))
                     .RuleFor(x => x.UntilYear, (faker, season) => untilYear ?? season.FromYear + faker.Random.Int(0, 1))
-                    .RuleFor(x => x.SeasonRoute, (faker, season) => $"/competition/season-faker/{season.SeasonId}/{season.FromYear}-{season.UntilYear}")
+                    .RuleFor(x => x.SeasonRoute, (faker, season) => season.UntilYear > season.FromYear
+                        ? $"{competition.CompetitionRoute}/{season.FromYear}-{season.UntilYear.ToString(CultureInfo.InvariantCulture).Substring(2)}"
+                        : $"{competition.CompetitionRoute}/{season.FromYear}")
                     .RuleFor(x => x.ResultsTableType, faker => faker.Random.Enum<ResultsTableType>())
                     .RuleFor(x => x.EnableRunsScored, faker => faker.Random.Bool())
                     .RuleFor(x => x.EnableRunsConceded, faker => faker.Random.Bool())
